56f3e8779fd65aa290afef61bc99c016bfd27911
OliverBryant <2713999266@qq.com>
2026-09-16 16:21:10 +0800

    chore: normalize lifecycle test imports

--- tests/web/services/test_durable_sandbox_lifecycle.py
+++ tests/web/services/test_durable_sandbox_lifecycle.py
@@ -9,8 +9,8 @@ import pytest
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 
-from xagent.web.models.sandbox import DurableSandboxLifecycle
 from xagent.sandbox.base import ExactGenerationProbe
+from xagent.web.models.sandbox import DurableSandboxLifecycle
 from xagent.web.services.durable_sandbox_lifecycle import (
     DurableLifecycleConflict,
     DurableSandboxDeleteCoordinator,
